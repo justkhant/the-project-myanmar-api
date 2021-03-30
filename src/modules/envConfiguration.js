66ead22bf24862e27f07710b6f0cce75d8a107e3
harryhtet <harryhtet@gmail.com>
2021-03-29 22:48:13 -0400

    base code & model created

---
+++ src/modules/envConfiguration.js
@@ -0,0 +1,25 @@
+/**
+ * @file envConfiguration.js
+ * @author Harry Htet
+ * @version 1.0
+ * @createdDate 03/28/2021
+ * @copyright Team Galone
+ */
+
+const envalid = require('envalid');
+const ENV_CHOICES = ['development', 'production', 'qa'];
+const { str, port } = envalid;
+
+// Sanitizing env values
+const env = envalid.cleanEnv(process.env, {
+  NODE_ENV: str({ choices: ENV_CHOICES }),
+  WEBSITES_PORT: port(),
+  WEBSITES_CONTEXT_PATH: str(),
+  WEBSITES_MONGODB_URL: str(),
+});
+
+module.exports = {
+  VAR_PORT: env.WEBSITES_PORT,
+  VAR_CONTEXT_PATH: env.WEBSITES_CONTEXT_PATH,
+  VAR_MONGODB_URL: env.WEBSITES_MONGODB_URL,
+};
